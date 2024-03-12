4c3319521166e63bb43aae1c5cf0ed5e0a51d2c2
vvbae <polarsatellitest@gmail.com>
2024-03-12 16:05:55 +0000

    add target tofino

---
+++ targets/tofino/program_info.h
@@ -0,0 +1,22 @@
+#ifndef BACKENDS_P4TOOLS_MODULES_P4RTSMITH_TARGETS_TOFINO_PROGRAM_INFO_H_
+#define BACKENDS_P4TOOLS_MODULES_P4RTSMITH_TARGETS_TOFINO_PROGRAM_INFO_H_
+
+#include "backends/p4tools/common/compiler/compiler_target.h"
+#include "backends/p4tools/modules/p4rtsmith/core/program_info.h"
+#include "ir/ir.h"
+#include "ir/node.h"
+
+namespace P4Tools::RTSmith::Tna
+{
+
+    class TofinoTnaProgramInfo : public ProgramInfo
+    {
+    public:
+        explicit TofinoTnaProgramInfo(const CompilerResult &compilerResult);
+
+        DECLARE_TYPEINFO(TofinoTnaProgramInfo);
+    };
+
+} // namespace P4Tools::RTSmith::Tna
+
+#endif /* BACKENDS_P4TOOLS_MODULES_P4RTSMITH_TARGETS_TOFINO_PROGRAM_INFO_H_ */
