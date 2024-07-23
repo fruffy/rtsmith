f8c0659f058335095bccb96436f49489a553090f
Zhanghan Wang <hankzhwang@gmail.com>
2024-07-22 21:23:03 -0400

    Add options for p4info output and control plane api

--- options.h
+++ options.h
@@ -37,6 +37,12 @@ class RtSmithOptions : public AbstractP4cToolOptions {
     /// @returns the path set with --generate-config.
     [[nodiscard]] std::optional<std::string> getConfigFilePath() const;
 
+    /// @returns the path set with --generate-p4info.
+    [[nodiscard]] std::optional<std::filesystem::path> p4InfoFilePath() const;
+
+    /// @returns the control plane API to use.
+    [[nodiscard]] std::string_view controlPlaneApi() const;
+
  private:
     // Write the generated config to the specified file.
     std::optional<std::filesystem::path> configFilePath = std::nullopt;
@@ -46,6 +52,12 @@ class RtSmithOptions : public AbstractP4cToolOptions {
 
     /// Whether to write the generated config to a file or to stdout.
     bool printToStdout_ = false;
+
+    // Write the P4Runtime control plane API description to the specified file.
+    std::optional<std::filesystem::path> _p4InfoFilePath = std::nullopt;
+
+    // The control plane API to use. Defaults to P4Runtime.
+    std::string _controlPlaneApi = "P4RUNTIME";
 };
 
 }  // namespace P4Tools
